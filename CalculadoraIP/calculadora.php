1d527613e65c8b7115c52b1b13a3d1af7443e1b5
suyxavier <suy.xavier@gmail.com>
2018-07-12 16:34:51 -0300

    Add files via upload

--- CalculadoraIP/calculadora.php
+++ CalculadoraIP/calculadora.php
@@ -1,83 +1,93 @@
 <?php
 
-    require_once "index.php";
+    
 
     function ChamarCalculadora($mascara,$parte4){
 
         $enderecos = pow(2,(32-$mascara));
         echo "A quantidade de endereços por sub-rede é $enderecos";
+        ?><br /><?php
 
         $subRede = 256 / $enderecos;
         echo "A quantidade de sub-rede é $subRede";
+        ?><br /><?php
 
         $host = $enderecos - 2;
         echo "A quantidade de endereços de host em cada sub-rede $host";
+		?><br /><?php
 
         $posicaoRede = (int)($parte4 / $enderecos);
         $primeiroEnd = $posicaoRede * $enderecos;
         $primeiroHost= $primeiroEnd + 1;
         echo "O primeiro endereço de host da sub-rede em que o endereço IP informado se encontra $primeiroHost";
+		?><br /><?php
 
         $ultimoHost = (int)($primeiroEnd + $enderecos -2);
         echo "O último endereço de host da sub-rede em que o endereço IP informado se encontra $ultimoHost";
+		?><br /><?php
 
         $broadcast = (int)($primeiroHost + $enderecos -2);
         echo "O endereço de broadcast da sub-rede em que o endereço informado se encontra $broadcast";
-
+		?><br /><?php
+	
         $maskDecimal = 256 - $enderecos;
         
         echo "A máscara da rede, em formato decimal $maskDecimal";
-
+		?><br /><?php
         $classes = $parte4;
 
 
        		if ($classes <= 126){
-       			echo "classe a";
+       			echo "Classe A";
+       			?><br /><?php
        	}
        		
 			if ($classes >= 128 AND $classes <= 191){
-				echo "classe b";
+				echo "Classe B";
+				?><br /><?php
 		}
  
- 
 			if ($classes >= 192 AND $classes <= 223){
-				echo "classe c";
-				
-
+				echo "Classe C";
+				?><br /><?php		
 		}
 		 	
 		 	if ($classes >= 224 AND $classes <= 239){
-				echo "classe d";
-				
+				echo "Classe D";
+				?><br /><?php
 		}
 
 		 	if ($classes >= 240 AND $classes <= 256){
-				echo "classe e";
-				
+				echo "Classe E";
+				?><br /><?php
 		}
 
-		//while (list(, $value) = each($arr)) {
-   				// echo "Value: $value<br />\n";
-		//	}
-
-        //classes A 1 até 126
-               //B 128 a 191
-               //C 192 a 223
-               //D 224 a 239
-               //E 240 a 255
-
-        //return $posicaoRede;
-        //$primeiroHost //= $posicaoRede + 1;
-        //$posicaoRede = (int)($subRede * $enderecos);
-        //$ultimoEnd = $posicaoRede / $enderecos;
-        //$ultimoHost= $ultimoEnd - 1;
-        //echo "a $ultimoHost";
-
+		switch ($classes){
+            case $classes >= 10 AND $classes <= 10.255:
+                echo  "O IP é privado";
+                ?><br /><?php
+                break;
+            case $classes >= 172.16 AND $classes <= 172.31:
+                echo "O IP é privado";
+                ?><br /><?php
+                break;
+            case $classes >=192.168 AND $classes <= 172.169:
+                echo "O IP é privado";
+                ?><br /><?php
+                break;
+            default:
+                echo "O IP é publico";
+                break;
+        }
     }
 
-    //$mascara = $_POST['mascara'];
-$mascara=26;
-$parte4 = 223;
-    $calculo = ChamarCalculadora($mascara, $parte4);
+    if (isset($_POST['mascara'])) {
+    	$mascara=$_POST['mascara'];
+		$parte4 = $_POST['parte4'];
+    	$calculo = ChamarCalculadora($mascara, $parte4);
+    };
 
 
+
+  
+?>
--- CalculadoraIP/index.php
+++ CalculadoraIP/index.php
@@ -7,22 +7,61 @@
 <!doctype html>
 <html lang="en">
 <head>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CALCULADORA IP</title>
+    <script>
+    	
+    	$(document).ready(function(){
+    		$("button").click(function(){
+    			if( ($("#1").val() == "") ||
+    				($("#2").val() == "") ||
+					($("#3").val() == "") ||
+					($("#4").val() == "") ||
+					($("#5").val() == "") )
+    				{
+    					alert('Preencha todos os campos!');
+
+    			}else{
+		    		$.post('calculadora.php',
+		    		{
+						parte1: $("#1").val(),
+						parte2: $("#2").val(),
+						parte3: $("#3").val(),
+						parte4: $("#4").val(),
+						mascara: $("#5").val(),
+					},
+					function(dados){
+		                $('.resultado').html(dados);
+		            });
+
+    			}
+
+        });
+    });
+
+    </script>
 </head>
 <body>
 
-<form action="calculadora.php" method="post">
-    <input style="border:1px solid red;   line-height: 2; padding: 0 5px; width: 200px;" type="text" size="3" maxlength="3" name="parte1">
-    <input style="border:1px solid green; line-height: 2; padding: 0 5px; width: 200px;"type="text" size="3" maxlength="3"  name="parte2">
-    <input style="border:1px solid green; line-height: 2; padding: 0 5px; width: 200px;"type="text" size="3" maxlength="3" name="parte3">
-    <input style="border:1px solid green; line-height: 2; padding: 0 5px; width: 200px;"type="text" size="3" maxlength="3" name="parte4">
-    <input style="border:1px solid green; line-height: 2; padding: 0 5px; width: 200px;"type="text" name="mascara" placeholder="Insira uma Máscara ">
-    <input tyle="border:1px solid green; background: green; color: #fff; font-weight: 700; cursor: pointer; line-height: 2; padding: 0 5px;" type="submit" name="enviar">
-</form>
+	<h1>Calculadora IP</h1>
 
+	<p> Esta é uma imprescindível ferramenta para técnicos e especialistas de rede. 
+
+	Com ela você pode calcular todos endereços que fazem parte de uma Classe IP, a partir de um endereço IP e sua respectiva Máscara. </p>
+
+	<h2>Endereço IP</h2>
+    <input id="1"" style="border:1px solid blue; line-height: 2; padding: 0 5px; width: 100px;"type="text" size="3"    maxlength="3" name="parte1">
+    <input id="2" style="border:1px solid blue; line-height: 2; padding: 0 5px; width: 100px;"type="text" size="3"    maxlength="3" name="parte2">
+    <input id="3" style="border:1px solid blue; line-height: 2; padding: 0 5px; width: 100px;"type="text" size="3"    maxlength="3" name="parte3">
+    <input id="4" style="border:1px solid blue; line-height: 2; padding: 0 5px; width: 100px;"type="text" size="3"    maxlength="3" name="parte4">
+
+    <h2>Máscara </h2>
+    <input id="5" style="border:1px solid green; line-height: 2; padding: 0 5px; width: 150px;"type="text" name="mascara" placeholder=" Máscara - Ex: /24 "> 
+    <button style="border:1px solid green; background: green; color: #fff; font-weight: 700; cursor: pointer; line-height: 2; padding: 0 5px;">Enviar</button>
+
+	<div class="resultado"></div>
+
+	<p>Desenvolvedores: Carolina Burckhardt Luciani, Rafaela Gilgen e Suyanne Xavier da Cunha</p>
 </body>
 </html>
